13716e151f4ed5a6c323187579712ef10456e8f7
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-29 14:25:28 +0530

    Open a pool for the database

---
+++ models/pool.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+const process = require("node:process");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = new Pool({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
