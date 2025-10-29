9a54a8b094c9aebeb7b22520ad90841c9a32afa4
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-29 18:41:45 +0530

    add query to get respective category based data

--- models/query.js
+++ models/query.js
@@ -11,9 +11,42 @@ async function getGames() {
     return rows;
 }
 
+async function getEachCategory() {
+    const { rows } = await pool.query("SELECT DISTINCT category AS category FROM category");
+    return rows;
+}
 
+async function gamesByCategoryGet(category) {
+
+    let sql = `
+        SELECT genre AS category, name AS title, global_rankings AS rank, cost_of_each_product AS price
+        FROM  games
+        JOIN stock ON games.id = game_id
+        JOIN game_rankings ON game_rankings.game_id = stock.game_id
+        GROUP BY genre, name, global_rankings, cost_of_each_product
+        ORDER BY genre, name, global_rankings, cost_of_each_product;
+    `
+
+    if(category === "company") {
+        sql = `
+            SELECT company AS category, name AS title, global_rankings AS rank, cost_of_each_product AS price
+            FROM game_company
+            JOIN stock ON game_company.game_id = stock.game_id
+            JOIN games ON games.id = stock.game_id
+            JOIN game_rankings ON game_rankings.game_id = games.id
+            GROUP BY company, name, global_rankings, cost_of_each_product
+            ORDER BY company, name, global_rankings, cost_of_each_product;
+        `
+    }
+
+    const { rows } = await pool.query(sql);
+
+    return rows;
+}
 
 
 module.exports = {
     getGames,
+    getEachCategory,
+    gamesByCategoryGet
 }
