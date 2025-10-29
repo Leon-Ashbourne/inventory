170a77267fdceedeccd67448267803e4707f5d40
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-29 18:40:50 +0530

    add controller for category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,14 +1,21 @@
-
-
+const db = require("../models/query");
 
 async function categoryGet(req, res) {
-    //
+    const categories = await db.getEachCategory();
+
+    res.render("category", {categories: categories, title: "Categories"});
+}
+
+async function byCategoryGet(req, res) {
+    const { name } = req.params;
+    const data = await db.gamesByCategoryGet(name);
+
+    res.render("categoryRender", {title: name, data: data})
 }
 
 
-
-
 module.exports = {
-    categoryGet
+    categoryGet,
+    byCategoryGet
 }
 
