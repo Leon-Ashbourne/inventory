c5863684dc27a07665d1410e533ca8eda0893c8b
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-29 14:27:48 +0530

    add controller to get data from the db

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,8 +1,8 @@
-// const db = require("../models/query");
+const db = require("../models/query");
 
 async function homeController(req, res) {
-    // const games = await db.getGames()
-    res.render("index", {games: [], title: "Games: "});
+    const games = await db.getGames()
+    res.render("index", {games: games, title: "Games: "});
 }
 
 
