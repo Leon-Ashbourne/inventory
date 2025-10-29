13425b99d88cba7492f62aebf0fdf30b2bb05564
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-30 00:42:57 +0530

    controller to add new game details

--- controller/createController.js
+++ controller/createController.js
@@ -1,17 +1,70 @@
+const db = require("../models/query");
+const { body, validationResult, matchedData } = require("express-validator");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//authenticate to add new game details
+exports.authenticateGet = (req, res) => {
+    res.render("auth/auth", {head: "Admin password is required: ", title: "authenticate"});
+};
 
 
-async function createGet(req, res) {
-    //
+function authenticatePost(req, res, next) {
+    const { password } = req.body;
+
+    if(password === process.env.AUTH_PASSWORD) {
+        console.log("success");
+        next();
+    }
+    else {
+        res.render("auth/error", {msg: "You entered a wrong password"});
+    }
+};
+
+function createGet (req, res) {
+    res.render("create/create", {title: "New game"});
 }
 
-async function createPost(req, res) {
-    //
-}
+
+// validate game details
+const nameErr = "must not be empty";
+const genreErr = "must only cotain alphabets";
+const relErr= "must only contain numbers";
+const restErr = "must be a positive number";
+
+const validate = [
+    body("name").trim()
+        .not().isEmpty().withMessage(`name ${nameErr}`),
+    body("genre").trim()
+        .isAlpha().withMessage(`genre ${genreErr}`),
+    body("released").trim()
+        .isInt().withMessage(`relase date ${relErr}`)
+        .custom((value) => {
+            const num = Number(value);
+            if(isNaN(num) || num < 0) throw new Error(`num ${restErr}`) ;
+            else return true;
+        })
+]
 
 
+exports.createPost = [
+    validate,
+    async (req, res) => {
+        // get more column values from the user
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).render("create", {
+                title: "Add Game",
+                errors: errors.array()
+            })
+        }
+        
+        const { name, genre, released } = matchedData(req);
+        await db.addGame({ name, genre, released });
 
-module.exports = {
-    createGet,
-    createPost
-}
+        res.redirect("/");
+    }
+]
 
+exports.authenticate = [authenticatePost, createGet]
