e05d3e755690c3c4c8cca4c2b1f94b73e1e2a852
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-30 00:12:02 +0530

    configure to recieve data on the app router

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 //setting views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({extended: true}));
 
 //router
 app.use("/create", createRouter);
