2eff9d68b8e7402a252b32956f8cc9ba54810de8
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-30 14:42:10 +0530

    update the route middleware functions for create route

--- routes/createRouter.js
+++ routes/createRouter.js
@@ -4,7 +4,7 @@ const createController = require("../controller/createController");
 const createRouter = Router();
 
 
-createRouter.post("/authenticate", createController.authenticate);
+createRouter.post("/authenticate", createController.authenticatePost, createController.createGet);
 createRouter.post("/", createController.createPost);
 createRouter.use(createController.authenticateGet);
 
