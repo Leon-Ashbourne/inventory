3217e256d15a2b75324f339b8906612e29b12c4a
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-31 22:00:15 +0530

    add queries to get each category values. Bug: can't enter the path /:name/:value

--- models/query.js
+++ models/query.js
@@ -44,16 +44,59 @@ async function gamesByCategoryGet(category) {
     return rows;
 }
 
+async function genreGet() {
+    const { rows } = await pool.query(`SELECT DISTINCT genre FROM games`);
+    return rows;
+}
+
+async function companyGet() {
+    const { rows } = await pool.query(`SELECT DISTINCT company FROM game_company`);
+    return rows;
+}
+
+async function yearGet() {
+    const { rows } = await pool.query(`SELECT DISTINCT released AS year FROM games`);
+    return rows;
+}
+
+async function categoriesGet() {
+    const { rows } = await pool.query(`SELECT category FROM category`);
+    return rows;
+}
 
 async function addGame({ name, genre, released }) {
     await pool.query(`INSERT INTO games (name, genre, released) VALUES ($1, $2, $3);`, [name, genre, released]);
     return;
 }
 
+async function categoryValuesGet(name, value) {
+    let sql = `
+        SELECT name, rank 
+        FROM games
+        WHERE ($1) = ($2);
+    `
+    if(name === "company") {
+        sql = `
+            SELECT name, rank
+            FROM games
+            RIGHT JOIN games_company ON game_id = games.id
+            WHERE ($1) = ($2); 
+        `
+    }
+    
+
+    const { rows } = pool.query(sql, [name, value]);
+    return rows;
+}
 
 module.exports = {
     getGames,
     getEachCategory,
     gamesByCategoryGet,
-    addGame
+    addGame,
+    genreGet,
+    companyGet,
+    yearGet,
+    categoriesGet,
+    categoryValuesGet
 }
