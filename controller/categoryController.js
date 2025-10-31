b1da2e249edaf787784f4b27157d4cb72dacb02c
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-10-31 21:57:50 +0530

    update middleware functions to accomadate with corresponding route queries

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,21 +1,69 @@
 const db = require("../models/query");
 
-async function categoryGet(req, res) {
+async function categoryGet(req, res, next) {
     const categories = await db.getEachCategory();
-
-    res.render("category", {categories: categories, title: "Categories"});
+    res.locals.categories = categories
+    next();
 }
 
-async function byCategoryGet(req, res) {
-    const { name } = req.params;
-    const data = await db.gamesByCategoryGet(name);
+async function valuesByCategoryGet(req, res, next) {
+    // write a more flexible code to get category wise details
+    const genre = await db.genreGet();
+    const year = await db.yearGet();
+    const company = await db.companyGet();
 
-    res.render("categoryRender", {title: name, data: data})
+    await Promise.all([genre, year, company]);
+
+    res.locals.categoryValues = [genre, year, company];
+    next();
 }
+
+function categoryValues(req, res) {
+    const [ genre, year, company ] = [...res.locals.categoryValues];
+    const { categories } = res.locals;
+
+    res.render("category/category", { title: "Available Categories: ", categories: categories, genre, year, company });
+}
+
+const categoryMid = [categoryGet, valuesByCategoryGet, categoryValues];
+
+async function byCategoryGet(req, res, next) {
+    const category = await db.categoriesGet();
+
+    res.locals.category = category.map((cg) => {
+        return cg.category;
+    });
+
+    next();
+}
+
+async function checkCategoryGet(req, res, next) {
+    const { name, value } = req.params;
+    const isContains = res.locals.category.includes(name);
+
+    if(!isContains) {
+        res.status(400).render("error", {title: "Error, entered a bad url. The request reeturned with a 400 status code.."})
+    }
+    else next();
+}
+
+async function valuesGet(req, res) {
+    const { name, value } = req.params;
+    let newName = name;
+
+    if(value === "rank") newName = "global_rankings"
+    else if(value === "year") newName = "released"
+
+    const categoryValues = await db.categoryValuesGet(newName, value);
+
+    res.render("category/values/values", {values: categoryValues, title:`Values under ${name} for ${value}`});
+}
+
+const categValMid = [byCategoryGet, checkCategoryGet, valuesGet ];
 
 
 module.exports = {
-    categoryGet,
-    byCategoryGet
+    categoryMid,
+    categValMid
 }
 
