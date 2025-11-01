be2409c0f39d07b949613af7536e45a49211a27c
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2025-11-01 11:00:00 +0530

    update param name from value -> id

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,7 +3,7 @@ const categoryController = require("../controller/categoryController");
 
 const categoryRouter = Router();
 
-categoryRouter.get("/:name/:value", categoryController.categValMid);
+categoryRouter.get("/:name/:id", categoryController.categValMid);
 categoryRouter.get("/", categoryController.categoryMid);
 
 
